cbf94af0b512467c8c993d50f38dfa39fd2e8593
Mia Blažeković <mia.blazekovic0@gmail.com>
2025-01-13 20:32:00 +0100

    Kreiranje novog projekta unutar Solutiona za objekte (klase) aplikacije

---
+++ NET/BookLog.Objects/Constants/ViewNaslovi.cs
@@ -0,0 +1,15 @@
+﻿namespace BookLog.Objects.Constants
+{
+    /// <summary>
+    /// Klasa za naslove Viewova aplikacije
+    /// Definirana je kao STATIC kako bi se omogućio pristup njezinim članovima bez potrebe za instanciranjem objekta klase
+    /// </summary>
+    public static class ViewNaslovi
+    {
+        public const string Login          = "Login";                // Naslov za KorisnickiRacun/Login
+        public const string Registracija   = "Registracija";         // Naslov za KorisnickiRacun/Registracija
+        public const string PregledProfila = "Profil";               // Naslov za KorisnickiRacun/PregledProfila
+        public const string HomeIndex      = "Naslovnica";           // Naslov za Home/Index
+        public const string Privacy        = "Politika privatnosti"; // Naslov za Home/Privacy
+    }
+}
