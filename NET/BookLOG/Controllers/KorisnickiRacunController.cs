8d630b4bbc6528fa5d59e03619bd6b0150347184
Mia Blažeković <mia.blazekovic0@gmail.com>
2025-01-14 18:14:13 +0100

    Dodavanje Controllera za Viewove korisničkog računa

---
+++ NET/BookLOG/Controllers/KorisnickiRacunController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookLOG.Controllers
+{
+    /// <summary>
+    /// Osnovna klasa koja omogućuje pristup funkcijama potrebnim za MVC obrazac
+    /// </summary>
+    public class KorisnickiRacunController : Controller
+    {
+        /// <summary>
+        /// Akcija koja se poziva kada korisnik zatraži stranicu za Login
+        /// </summary>
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
